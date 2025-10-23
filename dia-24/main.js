0833d3e81a3db0a993f64e05f51a2f774b02a0af
ibraim <ing.gonzalezvij@hotmail.com>
2025-10-23 15:17:33 -0600

    dia 24 agregado

--- dia-24/main.js
+++ dia-24/main.js
@@ -18,14 +18,12 @@ fetch("https://api.api-onepiece.com/v2/characters/en")
     .then((data) => {
         console.log(data);
         data.forEach(({id, name, age, bounty}) => {
-            if(id <= 10) {
                 personajesContainer.innerHTML += `
                 <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${name}</h5>
                     <p class="font-normal text-gray-700 dark:text-gray-400">Edad: ${age} Recompensa: ${bounty}</p>
                 </a>
                 `;
-            }
         });
     })
 
